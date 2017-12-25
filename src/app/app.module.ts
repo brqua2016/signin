ed48c04aa5774756bcd3a2c29a19369af023dbb9
NIADA <niada@vestas.com>
2017-12-24 19:07:36 -0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatInputModule, MatOptionModule, MatSelectModule} from '@angular/material';
+
 
 
 import { AppComponent } from './app.component';
@@ -8,10 +11,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LeaveComponent } from './leave/leave.component';
 import { SettingsComponent } from './settings/settings.component';
 import { EmergencyComponent } from './emergency/emergency.component';
+import { VisitorComponent } from './visitor/visitor.component';
 
 const appRoutes: Routes = [
   { path: '',   redirectTo: '/welcome', pathMatch: 'full' },
   {path: 'welcome', component: WelcomeComponent},
+  {path: 'visitor', component: VisitorComponent},
   {path: 'leave', component: LeaveComponent},
   {path: 'settings', component: SettingsComponent},
   {path: 'emergency', component: EmergencyComponent}
@@ -24,10 +29,17 @@ const appRoutes: Routes = [
     WelcomeComponent,
     LeaveComponent,
     SettingsComponent,
-    EmergencyComponent
+    EmergencyComponent,
+    VisitorComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
     RouterModule.forRoot(
       appRoutes
     )
